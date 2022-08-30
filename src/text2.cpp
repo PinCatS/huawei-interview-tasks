24c2c368d5dd4c0b5583fa4375f12413d425463c
Sergey Li <pincats@gmail.com>
2022-08-30 17:04:15 +0300

    fix: Change from int to unsigned int for text2

--- src/text2.cpp
+++ src/text2.cpp
@@ -6,26 +6,26 @@
 
 using namespace std;
 
-int ReadNumber() {
-  int number;
+unsigned int ReadNumber() {
+  unsigned int number;
   cin >> number;
   return number;
 }
 
-pair<int, int> ReadCountAndLimit() {
-  int numbers_count, limit;
+pair<unsigned int, unsigned int> ReadCountAndLimit() {
+  unsigned int numbers_count, limit;
   cin >> numbers_count >> limit;
   return {numbers_count, limit};
 }
 
-bool IsEndOfTestCases(const pair<int, int> &values) {
+bool IsEndOfTestCases(const pair<unsigned int, unsigned int> &values) {
   return (values.first == 0 and values.second == 0);
 }
 
 int main() {
-  int test_case_id = 1;
-  vector<int> numbers;
-  pair<int, int> count_and_limit;
+  unsigned int test_case_id = 1;
+  vector<unsigned int> numbers;
+  pair<unsigned int, unsigned int> count_and_limit;
   do {
     count_and_limit = ReadCountAndLimit();
     if (IsEndOfTestCases(count_and_limit)) {
@@ -33,11 +33,12 @@ int main() {
     }
 
     auto [count, limit] = count_and_limit;
-    for (int i = 0; i < count; ++i) {
+    for (unsigned int i = 0; i < count; ++i) {
       numbers.push_back(ReadNumber());
     }
 
-    int greatest_number = gn_huawei::FindGreatestNumber(numbers, limit);
+    unsigned int greatest_number =
+        gn_huawei::FindGreatestNumber(numbers, limit);
 
     cout << "Case "s << test_case_id << ": "s << greatest_number << endl
          << endl;
